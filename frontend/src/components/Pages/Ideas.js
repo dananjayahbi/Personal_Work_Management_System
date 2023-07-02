4bbe50107e1f4d4ea83e8db3bcf971af4b781428
djd <dananjayahbi@gmail.com>
2023-07-02 16:52:40 +0530

    add idea part done

--- frontend/src/components/Pages/Ideas.js
+++ frontend/src/components/Pages/Ideas.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../../styles/dashboard.css";
 import axios from "axios";
+import AddIdea from '../PageComponents/AddIdea';
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 
 export default function Ideas() {
@@ -24,11 +27,36 @@ useEffect(() => {
   }, 500);
 }, []);
 
+//The popup
+const [openPopup3, setOpenPopup3] = useState(false);
+
 
   return(
     <>
       <div className="d-flex justify-content-cneter">
-        Hello world
+        {/*Add User button*/}
+          <Button
+            startIcon={<AddIcon />}
+            variant="outlined"
+            sx={{
+              border: "1px solid #1e6907",
+              color: "#1e6907",
+              "&:last-child td, &:last-child th": { border: 0 },
+              "&:hover": {
+                backgroundColor: "#1e6907",
+                color: "var(--white)",
+                border: "1px solid var(--white)",
+              },
+              marginBottom: "25px",
+            }}
+            onClick={() => {
+              setOpenPopup3(true); // open the popup
+            }}
+          >
+            Add User
+          </Button>
+
+          <AddIdea openPopup3={openPopup3} setOpenPopup3={setOpenPopup3}></AddIdea>
       </div>
     </>
     
